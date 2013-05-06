109ed57041cd6bf9dbe90933150dd8a985cdce69
Mike Wright <mw@sb121.(none)>
2013-05-06 16:25:19 -0700

    try some path parsing

--- server.groovy
+++ server.groovy
@@ -1,11 +1,17 @@
+def logger = container.logger
 vertx.createHttpServer().requestHandler { req ->
-    def logger = container.logger
 
     logger.info "I am logging ${req.uri}"
+
     if(["/", "/index", "/home", "/index.html"].contains(req.uri)){
         req.response.sendFile "index.html"
     }else {
+        def pathChunks = req.path.tokenize("/")
+        if(pathChunks.count{it}>1){
 
+        } else {
+            logger.info "Only 1 pathChunk"
+        }
     }
 
 }.listen(8080)
