d3604433ebf6bb5cb1e84fb8adc1d90d0088712d
Mike Wright <mw@sb121.(none)>
2013-05-05 23:10:28 -0700

    typo

--- server.groovy
+++ server.groovy
@@ -1,4 +1,4 @@
-teHttpServer().requestHandler { req ->
+vertx.createHttpServer().requestHandler { req ->
     def file = req.uri == "/" ? "index.html" : req.uri
     req.response.sendFile "webroot/$file"
 }.listen(8080)
