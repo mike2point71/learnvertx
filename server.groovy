f6c10c3ad9bfc840fc41e7abc348074686dfd8b7
Mike Wright <mw@sb121.(none)>
2013-05-06 16:11:45 -0700

    Multiple ways to get home

--- server.groovy
+++ server.groovy
@@ -2,7 +2,7 @@ vertx.createHttpServer().requestHandler { req ->
     def logger = container.logger
 
     logger.info "I am logging ${req.uri}"
-    if(["/"].contains(req.uri)){
+    if(["/", "/index", "/home", "/index.html"].contains(req.uri)){
         req.response.sendFile "index.html"
     }else {
 
