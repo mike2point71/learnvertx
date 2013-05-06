4d721abac6dca975422e49261cb49be834b46069
Mike Wright <mw@sb121.(none)>
2013-05-05 23:13:36 -0700

    Try loggin

--- server.groovy
+++ server.groovy
@@ -1,4 +1,7 @@
 vertx.createHttpServer().requestHandler { req ->
+    def logger = container.logger
+
+    logger.info "I am logging ${req.uri}"
     def file = req.uri == "/" ? "index.html" : req.uri
     req.response.sendFile "webroot/$file"
 }.listen(8080)
